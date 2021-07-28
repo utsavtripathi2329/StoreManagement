13ec6def118cd9f60de5d43f79f198edc709f67d
utsavtripathi2329 <58241042+utsavtripathi2329@users.noreply.github.com>
2021-07-28 23:34:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 conn = connect(credentials=credentials)
 
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
